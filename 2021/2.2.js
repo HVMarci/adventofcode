d124af9694f9e96d43897fe184866820b41deec2
HVMarci <horvath.varga.marton@gmail.com>
2021-12-04 11:14:47 +0100

    2021/2/2

--- 2021/2.2.js
+++ 2021/2.2.js
@@ -1,19 +1,21 @@
-function f(arr) {
-    let depth = 0, hPos = 0;
+function fn(arr) {
+    let aim = 0, hPos = 0, depth = 0;
     for (let i = 0; i < arr.length; i++) {
         let ut = arr[i][0];
         let num = Number.parseInt(arr[i].split(" ")[1]);
         switch (ut) {
-            case 'f':
-                hPos += num;
+            case 'd':
+                aim += num;
                 break;
             case 'u':
-                depth -= num;
+                aim -= num;
                 break;
-            case 'd':
-                depth += num;
+            case 'f':
+                hPos += num;
+                depth += aim * num;
                 break;
         }
     }
-    return depth * hPos;
+
+    return hPos * depth;
 }
